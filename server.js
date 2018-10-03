af8ba97e79660785dedb6f68a604f7b8046ce525
Soumya <soumyacool2012@gmail.com>
2018-10-04 00:14:20 +0530

    Correct .gitignore, ES6ified server code

--- server.js
+++ server.js
@@ -1,60 +1,67 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 
-var app = express();
-app.use(morgan('combined'));
+const app = express();
+app.use(morgan("combined"));
 
-app.get('/',(req,res) => {
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 /*
 let py = new Promise( (resolve,reject) => {
 
-  var spawn = require("child_process").spawn;
-  var process = spawn('python',["./ML/predict.py"]);
+  const spawn = require("child_process").spawn;
+  const process = spawn('python',["./ML/predict.py"]);
 
   process.stdout.on('data', function (data){
     console.log(data.toString());
     resolve(data);
   });
 
-  
+
   process.stderr.on('data', function (data){
     console.log(data.toString());
     reject(data);
   });
-  
+
 
 });
 */
 
-app.get('/py',(req,res) => {
+app.get("/py", (req, res) => {
+  let out = "";
 
-let out = '';
+  const spawn = require("child_process").spawn;
+  const process = spawn("python", [
+    "./ML/ml.py",
+    "France",
+    619,
+    1,
+    42,
+    2,
+    0,
+    1,
+    1,
+    1,
+    101348.88
+  ]);
 
-var spawn = require("child_process").spawn;
-var process = spawn('python',["./ML/ml.py","France",619,1,42,2,0,1,1,1,101348.88]);
+  process.stdout.on("data", function(data) {
+    console.log(data.toString());
+    out = out + data.toString();
+  });
 
-process.stdout.on('data', function (data){
-  console.log(data.toString());
-  out = out + data.toString();
+  process.stdout.on("end", function() {
+    if (out.substring(2, 3) === "F") {
+      res.send("False");
+    } else {
+      res.send("True");
+    }
+  });
 });
 
-process.stdout.on('end', function(){
-  if(out.substring(2,3) === "F")
-  {
-    res.send("False");
-  }
-  else
-  {
-    res.send(("True"));
-  }
-});
-
-});
-
-var port = 7000;
-app.listen(port, function () {
+const port = 7000;
+app.listen(port, function() {
   console.log(`app listening on port ${port}!`);
 });
